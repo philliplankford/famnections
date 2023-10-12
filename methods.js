5d6afbff82b31dad808a9b850cbeb775bb929f0e
Phillip Lankford <phillip@Phillips-MacBook-Pro.local>
2023-10-11 20:13:21 -0700

    Main functions correct

--- methods.js
+++ methods.js
@@ -1,10 +1,10 @@
 const puzzle = {
     author: "YOUR NAME HERE",
     connections: {
-        "yellow": "YELLOW CONNECTION HERE",
-        "green": "GREEN CONNECTION HERE",
-        "blue": "BLUE CONNECTION HERE",
-        "purple": "PURPLE CONNECTION HERE"
+        "yellow": "3's",
+        "green": "Types of Traps",
+        "blue": "___ Of God",
+        "purple": "New"
     },
     key: {
         "trinity": "yellow",
@@ -27,10 +27,12 @@ const puzzle = {
 };
 /* SELECTORS */
 const grid = document.querySelector('#game-grid');
+const guess = document.querySelector('#guesses')
 
 /* VARIABLES */
 let words = Object.keys(puzzle.key);
 let selection = [];
+let mistakes = 4;
 
 /* METHODS */
 const shuffle = (array) => {
@@ -45,35 +47,86 @@ const countSelection = () => {
     return counts;
 };
 
+const solve = () => {
+    const color = puzzle.key[selection[0]];
+    selection.forEach((word) => {
+        console.log(word);
+        const ind = words.findIndex((e) => e === word);
+        console.log(ind);
+        words.splice(ind,1);
+    });
+
+    let solution = document.createElement("div");
+    let con = document.createElement("p");
+    let pieces = document.createElement("p");
+
+    con.textContent = puzzle.connections[color];
+    con.classList.add('stronk');
+    pieces.textContent = `${selection[0]}, ${selection[1]}, ${selection[2]}, ${selection[3]}`;
+    solution.appendChild(con);
+    solution.appendChild(pieces);
+    solution.classList.add('solution-box', `${color}`);
+    document.querySelectorAll(".word-box").forEach((e) => { e.remove(); });
+
+    grid.appendChild(solution);
+
+    appendWords();
+    selection = [];
+};
+
+const logMistake = () => {
+    mistakes--;
+    document.querySelectorAll(".guess-dot").forEach((e) => { e.remove(); })
+    appendGuesses();
+    console.log("try again..."); 
+};
+
 const checkConnection = () => {
     const colormatches = countSelection();
     if (Object.keys(colormatches).length === 2) {
         console.log("One away...");
+        logMistake();
     } else if (Object.keys(colormatches).length === 1) {
+        solve();
         console.log(`you found ${puzzle.connections[Object.keys(colormatches)[0]]}!`);
-    } else { console.log("try again...")}
+    } else { 
+        logMistake();    
+    }
+};
+
+const appendWords = () => {
+    shuffle(words).forEach((word) => {
+        let box = document.createElement("div");
+        box.textContent = word;
+        box.classList.add('word-box');
+        grid.appendChild(box);
+    });
+};
+
+const appendGuesses = () => {
+    for (let i=0; i < mistakes; i++) {
+        const dot = document.createElement("li");
+        dot.classList.add("guess-dot");
+        guess.appendChild(dot);
+    }
 };
 
 /* PROGRAM */
-shuffle(words).forEach((word) => {
-    let box = document.createElement("div");
-    box.textContent = word;
-    box.classList.add('word-box');
-    grid.appendChild(box);
-});
+appendWords();
 
+/* LISTENERS */
 grid.addEventListener('click', (e) => {
-    if (e.target.className == "word-box" && !e.target.classList.contains('selected')){
-        if (selection.length < 4) {
-            e.target.classList.add('selected');
+    if (mistakes >= 0) {
+        if (e.target.className == "word-box" && !e.target.classList.contains('selected')){
             selection.push(e.target.textContent);
+            e.target.classList.add('selected');
+            if (selection.length === 4) {
+                checkConnection();
+            }
+        } else {
+            e.target.classList.remove('selected')
+            let ind = selection.findIndex((element) => element === e.target.textContent);
+            selection.splice(ind, 1);
         }
-        if (selection.length === 4) {
-            checkConnection();
-        }
-    } else {
-        e.target.classList.remove('selected')
-        let ind = selection.findIndex((element) => element === e.target.textContent);
-        selection.splice(ind, 1);
     }
 });
